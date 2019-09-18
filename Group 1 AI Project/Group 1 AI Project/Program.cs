e3c01c46cb957737a2a14a6893c68a8319e1750b
clwsham <53279484+clwsham@users.noreply.github.com>
2019-09-19 11:31:20 +1200

    added an empty visual studio project to contribute to

---
+++ Group 1 AI Project/Group 1 AI Project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_1_AI_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //The most advanced AI ever concieved
+            //Microsoft has nothing on us
+
+        }
+    }
+}
