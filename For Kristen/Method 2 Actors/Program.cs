6bc12c01fe274b671073e7e127f6f9c9fa2cba1d
kristen collins <collikj1@student.op.ac.nz>
2019-09-25 20:21:05 +1200

    split actor/actresses

--- For Kristen/Method 2 Actors/Program.cs
+++ For Kristen/Method 2 Actors/Program.cs
@@ -21,7 +21,7 @@ namespace ConsoleApp10
                                  ,"Tom Hanks","Kirk Douglas","Mel Gibson","Vincent D'Onofrio","R. Lee Ermey","Adam Baldwin","Johnny Depp","Charlie Sheen","Willem Dafoe"
                                  ,"Gerard Butler","Michael Fassbender","Ralph Fiennes","Jeremy Renner"};
 
-            string[] warActresses = { "Lena Headey", "Evangeline Lilly" };
+            
 
             string[] dramaActors = {"Cary Grant","Bruce Willis","Haley Joel Osment","Leonardo DiCaprio","Joseph Gordon-Levitt",
                                     "Anthony Perkins","Matt Damon","Jack Nicholson","Mark Wahlberg","Morgan Freeman", "Brad Pitt", "Kevin Spacey"
@@ -29,7 +29,7 @@ namespace ConsoleApp10
                                     "Harvey Keitel","Tim Roth","Michael Madsen","Chris Penn","Russell Crowe"," Guy Pearce","Jason Flemyng",
                                     "Dexter Fletcher","Nick Moran","Jason Statham"};
 
-            string[] dramaActresses = {"Grace Kelly","Toni Collette","Janet Leigh","Jodie Foster","Kim Basinger" };
+           
 
             string[] scifiActors = {"Tom Hardy","George Clooney","Mark Hamill","Adam Driver", "John Boyega", "Adam Driver", "Harrison Ford",
                                     "Tom Skerritt","Harrison Ford","Alec Guinness","Chris Pine", "Zachary Quinto", "Leonard Nimoy", "Karl Urban","Arnold Schwarzenegger",
@@ -37,19 +37,17 @@ namespace ConsoleApp10
                                     "William Sylvester", "Daniel Richter", "Rutger Hauer","Edward James Olmos","Sam Worthington","Keanu Reeves", "Laurence Fishburne", "Joe Pantoliano",
                                     "Malcolm McDowell", "Patrick Magee","Aubrey Morris"};
 
-            string[] scifiActresses = {"Charlize Theron","Drew Barrymore","Sandra Bullock","Carrie Fisher","Daisy Ridley","Sigourney Weaver", "Linda Hamilton","Jessica Chastain",
-                                        "Kristen Wiig","Laura Dern","Sean Young","Zoe Saldana","Carrie-Anne Moss","Adrienne Corri"};
+           
 
             string[] horrorActors = {"Kevin Bacon", "Cillian Murphy", "Johnny Depp", "Anthony Hopkins", "Simon Pegg", "Nick Frost", "Chris Hemsworth", "Bill Skarsgard", "James McAvoy" };
 
-            string[] horrorActresses = { "Janet Leigh", "Linda Blair", "Sissy Spacek", "Jamie Lee Curtis", "Jodi Foster", "Naomi Watts", "Natalie Portman", "Kristen Connolly" };
+           
 
             string[] familyActors = {"Ian McKellen","Viggo Mortensen", "Orlando Bloom","Macaulay Culkin","Daniel Radcliffe", "Rupert Grint","Cary Elwes","Mandy Patinkin","Dan Stevens",
                                     "Luke Evans", "Ben Stiller","Max Records","Forest Whitaker", "Pepita Emmerichs","Johnny Depp", "Jim Carrey", "Jude Law", "Liam Aiken","Daniel Craig",
                                     "Johnny Depp","Tommy Lee Jones", "Will Smith"};
 
-            string[] familyActresses = {"Cate Blanchett","Liv Tyler","Emma Watson","Robin Wright","Angelina Jolie", "Elle Fanning","Carla Gugino","Catherine O'Hara","Helena Bonham Carter",
-                                        "Mia Wasikowska","Anne Hathaway","Meryl Streep","Nicole Kidman","Dakota Blue Richards","Kiera Knightly",};
+            
 
             string[] actionActors = {"Quentin Tarantino","Arnold Schwarzenegger","Edward Furlong","Keanu Reeves","Hugo Weaving","Laurence Fishburne","Bruce Willis","Alan Rickman","Harrison Ford",
                                      "Christian Bale","Michael Caine","Heath Ledger","Gary Oldman","Bill Paxton","Matt Damon","Robert Downey Jr.","Chris Evans","Mark Ruffalo","Chris Hemsworth",
@@ -57,8 +55,7 @@ namespace ConsoleApp10
                                         "Bradley Cooper","Paul Rudd","Michael Douglas","Corey Stoll","Benedict Cumberbatch","Chiwetel Ejiofor", "Tom Holland","Michael Keaton","Chadwick Boseman",
                                         "Jude Law"};
 
-            string[] actionActresses = {"Brie Larson","Rachel McAdams","Evangeline Lilly","Zoe Saldana","Natalie Portman","Scarlett Johansson","Famke Janssen","Maggie Grace","Uma Thurman",
-                                        "Lucy Liu","Linda Hamilton","Carrie-Anne Moss","Sigourney Weaver","Julia Stiles","Gwyneth Paltrow","Milla Jovovich"};
+           
 
 
             Console.Write("Type Answer: ");
